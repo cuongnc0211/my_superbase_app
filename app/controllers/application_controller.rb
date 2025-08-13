53bf1d0858b7fbd0efe5c34e246755394a5d1b4c
cuongnc0211 <cuongnguyenfu@gmail.com>
2025-08-13 07:32:02 +0700

    add basic authen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :basic_authenticate
+
+  private
+
+  def basic_authenticate
+    return unless Rails.env.staging? || Rails.env.development?
+
+    authenticate_or_request_with_http_basic("Restricted Area") do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
