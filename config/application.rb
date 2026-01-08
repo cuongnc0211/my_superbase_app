8b87527afc39fe5d3b977314235149991fd59d8d
cuongnc0211 <cuongnguyenfu@gmail.com>
2026-01-08 10:15:47 +0700

    add host prod

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module SuperBaseApp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.hosts << "mysuperbaseapp-production-4357.up.railway.app"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "mysuperbaseapp-production-4357.up.railway.app"
-
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
