4575ab64e4ebb04f2a874795ca2a97bde3ddb45a
cuongnc0211 <cuongnguyenfu@gmail.com>
2026-01-08 10:04:13 +0700

    add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "mysuperbaseapp-production-4357.up.railway.app"
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
